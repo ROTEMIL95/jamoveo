92155068cb8b6e5264057e3c05b99dc62b394ec9
ROTEMIL95 <56782793+ROTEMIL95@users.noreply.github.com>
2025-05-18 06:14:44 +0300

    תיקון MONGO_URI

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://jamoveoil.netlify.app', 
     credentials: true,
   });
 
